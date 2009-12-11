b1cdbccf3c4b107e9c36f28e6cacd35fbca6b991
PaulBendixen <PaulBendixen@715c86c6-e637-11de-87fd-f59fab27b0b2>
2009-12-11 09:46:57 +0000

    initial import

---
+++ Driver.java
@@ -0,0 +1,31 @@
+import lejos.nxt.Motor;
+//import lejos.nxt.*
+
+class Driver
+{
+	/** Constructor, sets the speed to run at */
+	Driver( int speed )
+	{
+	}
+
+	/** getReady venter til der gives signal til at starte */
+	void getReady()
+	{
+	}
+
+	/** Turns right if true */
+	void turn(boolean right)
+	{
+	}
+
+	/** instructs the tron bike to go forward
+	 * should do this almost all the time */
+	void goForward()
+	{
+	}
+
+	/** routine for terminating the tron bike */
+	void die()
+	{
+	}
+}
