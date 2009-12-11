166c32f9835bde5899c62ca189c7159515fca002
paulbendixen <paulbendixen@715c86c6-e637-11de-87fd-f59fab27b0b2>
2009-12-11 14:35:47 +0000

    Now I can go home and sleep, the Driver class is uploaded

--- trunk/Driver.java
+++ trunk/Driver.java
@@ -1,31 +1,42 @@
 import lejos.nxt.Motor;
 //import lejos.nxt.*
 
-class Driver
+public class Driver
 {
+	int speed;
+	private Motor motorRight = Motor.A;
+	private Motor motorLeft = Motor.B;
 	/** Constructor, sets the speed to run at */
 	Driver( int speed )
 	{
+		if (speed >0 && speed < 100)
+		{
+			this.speed = speed;
+		}
+		else
+		{
+			this.speed = 50;
+		}
 	}
 
-	/** getReady venter til der gives signal til at starte */
-	void getReady()
+	/** stops the tron bike so it is possible to wait for start, win or die*/
+	public void stop()
 	{
+		motorRight.stop();
+		motorLeft.stop();
 	}
 
 	/** Turns right if true */
-	void turn(boolean right)
+	public void turn(boolean right)
 	{
+		// Should we use the built in functions in Car.java or just do our own?
 	}
 
 	/** instructs the tron bike to go forward
 	 * should do this almost all the time */
-	void goForward()
+	public void goForward()
 	{
-	}
-
-	/** routine for terminating the tron bike */
-	void die()
-	{
+		motorRight.forward();
+		motorLeft.forward();
 	}
 }
