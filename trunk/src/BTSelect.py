35cdb144b3ebaf91eb68e0e2199bc810ddda19b9
paulbendixen <paulbendixen@715c86c6-e637-11de-87fd-f59fab27b0b2>
2009-12-29 22:31:50 +0000

    Added an option to select among bluetooth devices in the vicinity if tronControl is run with no parameters

---
+++ trunk/src/BTSelect.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import sys
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+
+# This is a dialog to open when looking for an NXT to connect to
+
+class BTSelect(QtGui.QDialog):
+	def __init__(self,items = [()],parent = None):
+		QtGui.QDialog.__init__(self,parent)
+
+		self.setWindowTitle('Select NXT')
+
+		self.retval = ""
+		self._items = items
+		self.table=QtGui.QTableWidget()
+		self.table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+		self.table.clear()
+
+		ok = QtGui.QPushButton('&Ok')
+		cancel = QtGui.QPushButton('&Cancel')
+
+		self.vbox = QtGui.QVBoxLayout()
+		self.hbox = QtGui.QHBoxLayout()
+		self.hbox.addStretch(1)
+		self.hbox.addWidget(ok)
+		self.hbox.addWidget(cancel)
+		self.vbox.addWidget(self.table)
+		self.vbox.addLayout(self.hbox)
+		self.setLayout(self.vbox)
+		self.connect(ok,QtCore.SIGNAL('clicked()'),self.accept)
+		self.connect(cancel,QtCore.SIGNAL('clicked()'),self.reject)
+		self.populate()
+
+
+	def populate(self):
+		self.table.setSortingEnabled(False)
+		self.table.setRowCount(len(self._items))
+		self.table.setColumnCount(2)
+		#self.table.setHorzontalHeaderLabels(['Address','Name'])
+		for row,item in enumerate(self._items):
+			for col,val in enumerate(item):
+				self.table.setItem(row,col,QtGui.QTableWidgetItem(val))
+	def address(self):
+		return self.retval
+
+	def accept(self):
+		selected = self.table.selectedItems()
+		if len(selected) > 0:
+			row = selected[0].row()
+			self.retval = self.table.item(row,0).text()
+			QtGui.QDialog.accept(self)
+		else:
+			QtGui.QMessageBox.warning(self,'This is a SELECT box',"You must select an address or a name before clicking ok")
+			return 0
+
+
+if __name__ == "__main__":
+	numbers = ['00:00:00:00:00:00', '00:16:53:04:00:1C', '00:16:53:03:5D:96']
+	names = ['noone', 'Odin', 'NXT']
+	app = QtGui.QApplication(sys.argv)
+	selector = BTSelect(zip(numbers,names))
+
+	if selector.exec_():
+		print "Returned ", selector.address()
